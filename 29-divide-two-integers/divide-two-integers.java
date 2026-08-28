09f815e4c350dc17a320286f7e6ef70c51eb88e5
Suman Pal <suman11pal02@gmail.com>
2026-08-28 22:27:50 +0530

    Time: 1 ms (95.65%) | Memory: 42.5 MB (68.70%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -1,25 +1,32 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-    if (dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
-        if (dividend == Integer.MIN_VALUE && divisor == 1) return Integer.MIN_VALUE;
-        
-        long a = Math.abs((long) dividend);
-        long b = Math.abs((long) divisor);
-        int result = 0;
-        
-        while (a >= b) {
-            long temp = b, multiple = 1;
-            while (a >= (temp << 1)) {
-                temp <<= 1;
+        if (dividend == Integer.MIN_VALUE && divisor == -1)
+        {
+            return Integer.MAX_VALUE;
+        }
+
+        boolean isNegative = (dividend < 0) ^ (divisor < 0);
+
+        int negDividend = dividend < 0 ? dividend : -dividend;
+        int negDivisor = divisor < 0 ? divisor : -divisor;
+
+        int quotient = 0;
+
+        while (negDividend <= negDivisor)
+        {
+            int tempDivisor = negDivisor;
+            int multiple = 1;
+
+            while (tempDivisor >= -1073741824 && (tempDivisor << 1) >= negDividend)
+            {
+                tempDivisor <<= 1;
                 multiple <<= 1;
             }
-            a -= temp;
-            result += multiple;
+
+            negDividend -= tempDivisor;
+            quotient += multiple;
         }
-        
-        return ((dividend > 0) == (divisor > 0)) ? result : -result;
+
+        return isNegative ? -quotient : quotient;
     }
-
 }
-
-    
